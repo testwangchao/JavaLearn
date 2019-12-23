1e27446973082dee910cbf39c3f0d991d9947a7c
wangchao <15201403874@163.com>
2019-12-24 00:10:59 +0800

    init

--- src/AbstractClass/InterfaceClass.java
+++
@@ -1,8 +0,0 @@
-package AbstractClass;
-
-/**
- * Created by Administrator on 2017/7/6.
- */
-public interface InterfaceClass {
-    public void func();
-}
--- src/InterfaceClass/InterfaceTest1.java
+++
@@ -1,8 +0,0 @@
-package InterfaceClass;
-
-/**
- * Created by Administrator on 2017/7/6.
- */
-interface InterfaceTest1 {
-    void func1(int a);
-}
