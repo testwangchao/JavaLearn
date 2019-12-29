d74650912ca6a39f195ea76bff8cb56e8a9a4800
wangchao <15201403874@163.com>
2019-12-29 18:11:37 +0800

    add hooks

--- apps/cms/hooks.py
+++ apps/cms/hooks.py
@@ -2,6 +2,7 @@ from .views import bp
 from flask import session
 from config import CMS_USER_ID
 from .models import CmsUser
+from .models import CMSPermission
 from flask import g
 
 
@@ -12,3 +13,8 @@ def before_request():
         user = CmsUser.query.get(user_id)
         if user:
             g.cms_user = user
+
+
+@bp.context_processor
+def cms_context_processor():
+    return {"CMSPermission": CMSPermission}
